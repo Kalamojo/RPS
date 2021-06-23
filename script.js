6cddb6a04ddf239f80050a3c57ef012fff732bab
Kalamojo <kojo22444488888888@gmail.com>
2021-06-23 14:55:55 -0500

    Yay. RPS is functional

--- script.js
+++ script.js
@@ -8,4 +8,59 @@ function computerPlay() {
         return "Scissors";
     }
 }
-console.log(computerPlay());
+
+
+
+function game() {
+    
+    let compScore = 0;
+    let myScore = 0;
+
+    function round() {
+        for (let i = 0; i <= 5; i++) {
+            
+            let umg = prompt("Rock, paper, or scissors?", "Rock");
+            let playerSelection = umg.toLowerCase();
+            let computerSelection = computerPlay();
+            
+            if (playerSelection == "rock" && computerSelection == "Paper") {
+                console.log("You Lose! Paper beats Rock");
+                compScore += 1;
+            } else if (playerSelection == "paper" && computerSelection == "Rock") {
+                console.log("Yay! You Won! Paper beats Rock");
+                myScore += 1;
+            } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+                console.log("You Lose! Scissors beats Paper");
+                compScore += 1;
+            } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+                console.log("Yay! You Won! Scissors beats Paper");
+                myScore += 1;
+            } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+                console.log("You Lose! Scissors beats Rock");
+                compScore += 1;
+            } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+                console.log("Yay! You Won! Rock beats Scissors");
+                myScore += 1;
+            }
+            else {
+                console.log("Its a draw! Play another round");
+                i -= 1;
+            }
+        }
+
+    }
+    
+    round();
+
+    if (compScore > myScore) {
+        console.log("Ooooooof. Sucks to suck man. Computer Wins.");
+    } else if (myScore > compScore) {
+        console.log("Lets Gooooooooo! You won! You are literally the best.")
+    }
+    else {
+        console.log("Um. Okay then.");
+    }
+
+}
+
+game();
