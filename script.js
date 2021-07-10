ab50f1364c2744041d3d1145b3b03b8ac8737feb
Kalamojo <kalabiprof@gmail.com>
2021-07-10 14:29:15 -0500

    HTML connections and fully functional game series.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let rock = document.querySelector('#rock');
 let paper = document.querySelector('#paper');
 let scissors = document.querySelector('#scissors');
 
+let results = document.querySelector('#results');
+let comScore = document.querySelector('#comResults');
+let meScore = document.querySelector('#meResults');
+let EOG = document.querySelector('#EOG');
+
 function computerPlay() {
     let number = Math.random() * 3;
     if (number / 2 < 0.75) {
@@ -57,18 +62,30 @@ function round(playerSelection) {
         times -= 1;
     }
 
+    comScore.textContent = compScore;
+    meScore.textContent = myScore;
+
     times += 1;
 
-    if (times >=5) {
+    if (compScore >= 5 || myScore >= 5) {
         if (compScore > myScore) {
             console.log("Ooooooof. Sucks to suck man. Computer Wins.");
+            results.textContent = "Ooooooof. Sucks to suck man. Computer Wins.";
         } else if (myScore > compScore) {
             console.log("Lets Gooooooooo! You won! You are literally the best.")
+            results.textContent = "Lets Gooooooooo! You won! You are literally the best.";
         } else if (myScore == compScore) {
             console.log("No shot. A draw. There Must be some bugs in this.");
+            results.textContent = "No shot. A draw. There Must be some bugs in this.";
         }
         else {
             console.log("Um. Okay then.");
+            results.textContent = "Um. Okay then.";
         }
+        rock.style.visibility = "hidden";
+        paper.style.visibility = "hidden";
+        scissors.style.visibility = "hidden";
+
+        EOG.textContent = "Please the reload the page to play again.";
     }
 }
